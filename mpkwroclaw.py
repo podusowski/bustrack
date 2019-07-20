c53ec098ea89e186ee889701fad93e2d7d4b86aa
Piotr Podusowski <podusowski@gmail.com>
2019-07-20 22:01:53 +0200

    some progress on fetching data

--- mpkwroclaw.py
+++ mpkwroclaw.py
@@ -1,7 +1,21 @@
 import requests
+import time
+from typing import NamedTuple
+from collections import defaultdict
 
 
 _URL = 'http://mpk.wroc.pl/position.php'
+
+
+class VehicleIdentity(NamedTuple):
+    line: int
+    course: int
+
+
+class Position(NamedTuple):
+    x: int
+    y: int
+    timestamp: int
 
 
 def fetch_positions(buses):
@@ -11,6 +25,19 @@ def fetch_positions(buses):
     return r.json()
 
 
+def print_database(db):
+    for identity, samples in db.items():
+        print(f'{identity}: {len(samples)} samples')
+
+
 if __name__ == "__main__":
-    p = fetch_positions([132, 107])
-    print(p)
+    vehicles = defaultdict(list)
+    while True:
+        p = fetch_positions([132, 107])
+        for sample in p:
+            identity = VehicleIdentity(line=sample['name'], course=sample['k'])
+            position = Position(x=sample['x'], y=sample['y'], timestamp=0)
+            vehicles[identity].append(position)
+            #print(vehicles)
+        print_database(vehicles)
+        time.sleep(1)
