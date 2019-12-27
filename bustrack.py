46b0c4ec5b425ae5ab4e62370c3234bb75a795b8
Piotr Podusowski <podusowski@gmail.com>
2019-12-27 21:35:08 +0100

    feed support

--- bustrack.py
+++ bustrack.py
@@ -2,7 +2,7 @@
 import argparse
 import time
 import datetime
-from mpkwroclaw import OpenData
+from mpkwroclaw import OpenData, FEEDS
 
 
 def _parse_args():
@@ -11,6 +11,7 @@ def _parse_args():
 
     record_parser = subparsers.add_parser('record', help='record line\'s position')
     record_parser.add_argument('--line', type=int, required=True)
+    record_parser.add_argument('--feed', type=str, default='mpk', help='where take the data from', choices=['mpk', 'opendata'])
 
     return parser.parse_args()
 
@@ -18,10 +19,10 @@ def _parse_args():
 def _record(args):
     print(f'# recording started at {datetime.datetime.now()}')
     print('$ time;identity;line;position')
-    feed = OpenData()
+    feed = FEEDS[args.feed](args.line)
     while True:
         for record in feed:
-            print(f'{record.time};{record.side_number};{record.line};{record.lat},{record.lon}')
+            print(f'{record.time};{record.identity};{record.line};{record.lat},{record.lon}')
         time.sleep(5)
 
 
--- mpkwroclaw.py
+++ mpkwroclaw.py
@@ -33,18 +33,36 @@ class OpenDataRecord(NamedTuple):
     lon: float
     time: str
 
+    @property
+    def identity(self):
+        return self.side_number
+
 
 class OpenData:
     _URL = 'https://www.wroclaw.pl/open-data/datastore/dump/17308285-3977-42f7-81b7-fdd168c210a2'
 
+    def __init__(self, lines):
+        self._lines = lines
+
     def __iter__(self):
         r = requests.get(OpenData._URL)
-        print(r.text)
         data = r.text
         for line in data.splitlines()[1:]:  # 1st line is a header
             record = OpenDataRecord(*line.split(','))
-            if record.line != 'None':
+            if record.line in self._lines:
                 yield record
+
+
+class MpkRecord(NamedTuple):
+    course: int
+    line: str
+    lat: float
+    lon: float
+    time: str
+
+    @property
+    def identity(self):
+        return f'{self.line}[{self.course}]'
 
 
 class Mpk:
@@ -59,9 +77,10 @@ class Mpk:
         post_data = {'busList[bus][]': self._lines}
         r = requests.post(Mpk._URL, data=post_data)
         for sample in r.json():
-            identity = VehicleIdentity(line=sample['name'], course=sample['k'])
-            position = Position(x=sample['x'], y=sample['y'])
-            yield identity, position
+            yield MpkRecord(course=sample['k'], line=sample['name'], lat=sample['x'], lon=sample['y'], time=datetime.datetime.now())
+
+
+FEEDS = {'opendata': OpenData, 'mpk': Mpk}
 
 
 def _display_tavel_time_db(db, start, stop):
