b46fe4d81d26a03f1b6a8a6c9855108deae6cee9
Piotr Podusowski <podusowski@gmail.com>
2020-01-06 21:15:21 +0100

    iterator draft

--- rustrack/src/main.rs
+++ rustrack/src/main.rs
@@ -1,17 +1,94 @@
 use std::io::{self, Read};
+use std::collections::HashMap;
+use std::iter::Iterator;
 
 struct Record
 {
+    line: u32,
     lat: f32,
     lon: f32
 }
 
 impl Record
 {
-    fn from_string(s: &String) -> Record
+    fn from_string(s: &str) -> Record
     {
-        Record{lat: 0.0, lon: 0.0}
+        Record{line: 0, lat: 0.0, lon: 0.0}
     }
+}
+
+struct Ecsv<Input> where Input: Iterator
+{
+    input: Input,
+    fmt: Vec::<String>
+}
+
+impl<T> Ecsv<T> where T: Iterator
+{
+    fn new(reader: T) -> Ecsv<T>
+    {
+        Ecsv::<T>{input: reader, fmt: Vec::new()}
+    }
+}
+
+impl<T> Iterator for Ecsv<T> where T: Iterator
+{
+    type Item = (String, String);
+
+    fn next(self: &mut Self) -> Option<(String, String)>
+    {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests
+{
+    #[test]
+    fn name() {
+        let input = vec!["$x;y", "1,2", "3,4"];
+        let ecsv = Ecsv::new(input);
+    }
+}
+
+// TODO: implement this as an Iterator
+fn read_ecsv(input: &mut std::io::BufRead) -> HashMap::<String, String>
+{
+    let mut buf = String::new();
+    let mut fmt = Vec::<String>::new();
+    let mut ret = HashMap::<String, String>::new();
+
+    loop
+    {
+        let r = input.read_line(&mut buf);
+        match r
+        {
+            Ok(0) => break,
+            Err(_) => break,
+            _ => ()
+        }
+
+        if buf.starts_with("#")
+        {
+            continue
+        }
+        else if buf.starts_with("$")
+        {
+            fmt = buf.trim().split(";").map(|s| s.to_string()).collect();
+        }
+        else
+        {
+            let items = buf.split(";");
+            let items = fmt.iter().zip(items);
+            for (k, v) in items
+            {
+                // TODO: how to do this without string deep-copy?
+                ret.insert(k.to_string(), v.to_string());
+            }
+        }
+    }
+
+    ret
 }
 
 fn main() -> io::Result<()>
