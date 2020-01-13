8be9f91c8b2c6f8e9becc27eb7ccbecc71931e60
Piotr Podusowski <podusowski@gmail.com>
2020-01-13 20:30:18 +0100

    reading

--- rustrack/src/main.rs
+++ rustrack/src/main.rs
@@ -1,32 +1,45 @@
+use std::collections::HashMap;
 use std::io::{self, BufRead};
 use std::iter::Iterator;
 
-#[macro_use] extern crate maplit;
+#[macro_use]
+extern crate maplit;
 
 mod ecsv;
 use ecsv::Ecsv;
 
-struct Record
-{
+#[derive(Debug)]
+struct Record {
     line: u32,
     lat: f32,
-    lon: f32
+    lon: f32,
 }
 
-impl Record
-{
-    fn from_string(s: &str) -> Record
-    {
-        Record{line: 0, lat: 0.0, lon: 0.0}
+#[derive(Debug)]
+struct RecordParseError;
+
+impl From<std::num::ParseIntError> for RecordParseError {
+    fn from(_: std::num::ParseIntError) -> RecordParseError {
+        RecordParseError {}
     }
 }
 
-fn main() -> io::Result<()>
-{
+impl Record {
+    fn from_map(map: HashMap<String, String>) -> Result<Record, RecordParseError> {
+        Ok(Record {
+            line: map["line"].parse::<u32>()?,
+            lat: 0.0,
+            lon: 0.0,
+        })
+    }
+}
+
+fn main() -> io::Result<()> {
     let input = io::stdin();
     let lines = input.lock().lines().filter_map(Result::ok);
 
     for elem in Ecsv::new(lines) {
+        let elem = Record::from_map(elem);
         println!("{:?}", elem);
     }
 
